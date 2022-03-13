c83f92c7f5655c3fb0ad1d07613dccac407713e8
Marcelo de Carvalho Santana <marcelo.scientist@hotmail.com>
2022-03-13 16:09:03 -0300

    Controller w/ initial url setup & redirect routes

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const URLController_1 = require("./controller/URLController");
 const api = (0, express_1.default)();
+api.use(express_1.default.json());
+const urlController = new URLController_1.URLController();
+api.post('/shorten', urlController.shorten);
+api.get('/:hash', urlController.redirect);
 api.listen(5000, () => {
     console.log('express listing');
 });
